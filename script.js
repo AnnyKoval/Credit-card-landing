b73238ed9cdf653f2dcb1b4c6f9a17e2eda15037
Anna Koval <106483373+AnnyKoval@users.noreply.github.com>
2022-06-28 00:31:39 +0300

    Updated pages, added JS functions
    Updated pages (added information), added JS functions - onclick buttons

---
+++ script.js
@@ -0,0 +1,14 @@
+function goURLYoutube() {
+  document.location.href =
+    "https://youtube.com/watch?v=s5HrEyEStYE&ab_channel=VisaSchoolofPublicPolicy%28VSPP%29";
+}
+
+function goURLGoogle() {
+  document.location.href = "https://google.com";
+}
+function goURLAmazon() {
+  document.location.href = "https://amazon.com";
+}
+function goURLSlack() {
+  document.location.href = "https://slack.com";
+}
